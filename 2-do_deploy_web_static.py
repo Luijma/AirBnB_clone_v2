c91501f2c07eb92b6bc5b66cd5b6df80d3d410d1
Luijma <rsluismanuel@gmail.com>
2022-04-28 23:15:54 +0000

    t2 of deployment

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" Generates a .tgz archive from contents in web_static
+utilizing function do_pack """
+
+from fabric.api import run, put
+import os
+from os import path
+
+os.environ.hosts = ["35.229.80.162", "54.234.73.229"]
+
+
+def do_pack():
+    """ Compress to .tgz files """
+    if path.exists(archive_path):
+        try:
+            put(archive_path, '/tmp/')
+            file_name = archive_path[9:]
+            unc_file = '/data/web_static/releases/' + filename[:-4] + '/'
+
+            # Following commands based on hbton output
+
+            # Create directory if doesn't exist
+            run('mkdir -p ' + unc_file)
+
+            # Uncompress file_name
+            run('tar -xzf /tmp/' + file_name + ' -C ' + unc_file)
+
+            # Remove file_name from temp folder
+            run('rm -f /tmp/' + file_name)
+            
+            # Move files in web_static to directory ?
+            run('mv ' + unc_file + '/web_static/* ' + unc_file)
+
+            # Delete symbolic link
+            run('rm -rf /data/web_static/current')
+
+            # Creates new symb link
+            run('ln -s ' + final_name + '/data/web/static/current')
+
+            print('New version deployed!')
+            return True
+        except:
+            return False
+    else:
+        return False
