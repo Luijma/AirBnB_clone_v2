0456000453678c197cc5ce2c294dbf720233f9ce
Luijma <rsluismanuel@gmail.com>
2022-04-28 21:46:20 +0000

    t0 of deploymnet

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# set up to deploy webstatic
+
+new_strings="\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared
+echo "simple content" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i "40 i $new_strings" /etc/nginx/sites-enabled/default
+sudo service nginx restart
