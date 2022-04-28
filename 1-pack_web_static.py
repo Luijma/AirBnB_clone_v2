2c64cc919d9afb9468496aa4f9fa4e4c1603234b
Luijma <rsluismanuel@gmail.com>
2022-04-28 22:39:24 +0000

    t1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Generates a .tgz archive from contents in web_static
+utilizing function do_pack """
+
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """ Compress to .tgz files """
+    try:
+        local("mkdir -p versions")
+        now = datetime.now()
+        date = now.strftime("%Y%m%d%H%M%S")
+        file_name = "web_static_{}.tgz".format(date)
+        local("tar -cvzf versions/{} web_static".format(file_name))
+        return file_name
+    except:
+        return None
