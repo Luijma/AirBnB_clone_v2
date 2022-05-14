1d96556ab4b17383835803039e0f337baaaa93da
Luijma <rsluismanuel@gmail.com>
2022-05-14 20:56:07 +0000

    t2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Starts flask application
+"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def hbnb(text):
+    ''' prints text variable '''
+    result = text.replace('_', ' ')
+    return "C {}".format(result)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
